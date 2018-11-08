431fa01b16ef74393f9b4693096783905f3a7758
Matheus Vieira <130126861@aluno.unb.br>
2018-11-08 16:24:30 -0200

    Passing Data to Components
    Passar data pros componentes usando o objeto props
    
    Começa em 1:21:20

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.handleIncrement = this.handleIncrement.bind(this); //bind event handler (setar o this)
   }
 
@@ -27,30 +26,18 @@ class Counter extends Component {
   }
 
   handleIncrement() {
-    this.setState({ count: this.state.count + 1 }); //this.state.count++ nao funciona pq temos que avisar pro React que o State mudou
-  }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+    this.setState({ value: this.state.value + 1 }); //this.state.count++ nao funciona pq temos que avisar pro React que o State mudou
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
