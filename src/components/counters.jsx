a3f777a31de1fbe0803f93793f851c69f5ed6ac8
Matheus Vieira <130126861@aluno.unb.br>
2018-11-12 23:50:20 -0200

    Single Source of Truth / Controlled Components
    Começa em 1:44:00

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,22 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; // "..." clones the array
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleDelete = idCounter => {
     const newCounters = this.state.counters.filter(
       c => c.idCounter !== idCounter
@@ -21,10 +37,18 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        <br />
         {this.state.counters.map(counter => (
           <Counter
             key={counter.idCounter}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
