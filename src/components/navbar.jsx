7f2f1cb08518ef7ee4fe7ad3e0456027b9fafee4
Matheus Vieira <130126861@aluno.unb.br>
2018-11-13 15:04:17 -0200

    Destructing Arguments
    Começa em 2:08:50

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 
 // stateless functional component (quando a classe nao tem nenhum estado ou metodo, podemos usar uma funcao ao inves de classe)
-
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Carrinho de Compras{" "}
-        <span className="badge-pill badge-secondary">
-          {props.totalCounters}
-        </span>
+        <span className="badge-pill badge-secondary">{totalCounters}</span>
       </a>
     </nav>
   );
